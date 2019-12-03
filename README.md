b120116d6cd354b36397feda381682a3cd6a254f
Craig <sgccarey@gmail.com>
2019-12-03 13:26:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ONNX-Runtime examples
+
+## Setup
+
+Set python to python3 as default
+```bash
+sudo ln -sfn /usr/bin/python3 /usr/bin/python
+```
+
+Create & activate virtual environment
+```bash
+pip3 install --user virtualenv
+virtualenv venv
+source venv/bin/activate
+```
+
+Install requirements
+```bash
+apt install python3-pip
+pip install -r requirements.txt
+```
---
+++ simple_onnxruntime_inference.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import onnxruntime
+import numpy as np
+from onnxruntime.datasets import get_example
+
+example_model = get_example("sigmoid.onnx")
+sess = onnxruntime.InferenceSession(example_model)
+
+# identify our input name and shape
+input_name = sess.get_inputs()[0].name
+print("Input name  :", input_name)
+input_shape = sess.get_inputs()[0].shape
+print("Input shape :", input_shape)
+input_type = sess.get_inputs()[0].type
+print("Input type  :", input_type, "\n")
+
+# identify our output name and shape
+output_name = sess.get_outputs()[0].name
+print("Output name  :", output_name)
+output_shape = sess.get_outputs()[0].shape
+print("Output shape :", output_shape)
+output_type = sess.get_outputs()[0].type
+print("Output type  :", output_type, "\n")
+
+# pass in some input and compute our predictions
+x = np.random.random(input_shape)
+x = x.astype(np.float32)
+
+print("x:\n", x, "\n")
+
+result = sess.run([output_name], {input_name: x})
+
+print("result:\n", result, "\n")
